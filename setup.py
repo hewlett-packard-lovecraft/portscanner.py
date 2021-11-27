969be9a239b255498c0b98d1f365a97c19b2586f
Haoming Xia <mrmist3r311@gmail.com>
2021-11-27 01:45:50 -0800

    edited description

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='portscanner.py',
+    description="Python port scanner",
     version='0.0.1',
     packages=['portscannerpy'],
     install_requires=[
